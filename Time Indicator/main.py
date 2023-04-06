687496ae4159071dc88e0070eeec04a3decf73af
Aastik <aastikgakhar01@gmail.com>
2023-04-06 05:50:35 +0530

    Created New project

---
+++ Time Indicator/main.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template ('Time indicator.html')
+
+if __name__=="__main__":
+    app.run(port=6115)
